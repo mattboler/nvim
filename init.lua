1bdffe0c9878249d2e6d38f44a8edbcc66ae0090
Matt Boler <failtolaunch28@gmai.com>
2022-01-20 20:40:16 -0600

    Better comments

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local cmd = vim.cmd
 local opt = vim.opt
 local g = vim.g
 
--- Package Mangement
+---- Package Mangement
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -11,27 +11,42 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 require "paq" {
-  -- Package Manageer
+  ---- Package Manageer
 	"savq/paq-nvim"; 
-  -- UI/Colorscheme
+
+  ---- UI/Colorscheme
+  -- Base16 colorschemes for NeoVIM
   "RRethy/nvim-base16";
-  'mhinz/vim-signify'; -- Show git signs in gutter
-  'mhinz/vim-startify'; -- Startup screen
-  -- Language support
-  'nvim-treesitter/nvim-treesitter'; -- Treesitter
-  -- Fuzzy Finding
+
+  -- Add git status symbols to the gutter
+  'mhinz/vim-signify';
+
+  -- Show a recent files screen on startup
+  'mhinz/vim-startify';
+
+  -- Activate NeoVIM's improved syntax highlighting
+  'nvim-treesitter/nvim-treesitter';
+
+  ---- Functionality
+  -- Add fuzzy-finding in popup window
+  "nvim-telescope/telescope.nvim";
+
+  ---- Language-specific
+  -- Add latex->unicode support in Julia
+  'JuliaEditorSupport/julia-vim';
+
+  ---- Etc
   "nvim-lua/plenary.nvim"; -- For telescope
-  "nvim-telescope/telescope.nvim"; -- Fuzzy finder
 }
 
--- UI Configuration
-opt.termguicolors = true -- Enable 256-color
+---- UI Configuration
+opt.termguicolors = true                   -- Enable 256-color
 cmd 'colorscheme base16-gruvbox-dark-pale' -- Set colorscheme
-opt.number = true -- Show line numbers
-opt.scrolloff = 8 -- Rows of contet when when scrolling
-opt.sidescrolloff = 8 -- Columns of context when scrolling
-opt.splitbelow = true -- New hsplits are below current window
-opt.splitright = true -- New vsplits are to right of current window
+opt.number = true                          -- Show line numbers
+opt.scrolloff = 8                          -- Rows of contet when when scrolling
+opt.sidescrolloff = 8                      -- Columns of context when scrolling
+opt.splitbelow = true                      -- New hsplits are below current window
+opt.splitright = true                      -- New vsplits are to right of current window
 
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
@@ -43,20 +58,20 @@ g.signify_sign_add='+'
 g.signify_sign_delete='-'
 g.signify_sign_change='|'
 
--- Indentation
-opt.expandtab = true -- Use spaces instead of tabs
-opt.tabstop = 2 -- A tab is 2 spaces
-opt.shiftwidth = 2 -- An indent is 2 spaces
-opt.shiftround = true -- Round indents
+---- Indentation
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.tabstop = 2        -- A tab is 2 spaces
+opt.shiftwidth = 2     -- An indent is 2 spaces
+opt.shiftround = true  -- Round indents
 opt.smartindent = true -- Automatically indent
 
--- Searching
-opt.hlsearch = true -- Highlight matches
-opt.incsearch = true -- Match as you type
+---- Searching
+opt.hlsearch = true   -- Highlight matches
+opt.incsearch = true  -- Match as you type
 opt.ignorecase = true -- Case-insensitive matching
-opt.smartcase = true -- ... unless you type in mixed case
+opt.smartcase = true  -- ... unless you type in mixed case
 
--- Keybinds
+---- Keybinds
 g.mapleader = ";"
 vim.api.nvim_set_keymap('n', '<Leader>ff', [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], {noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>fg', [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], {noremap = true, silent = true })
