c6b7770bc7af22bfa35ec19a0ba764e1d3fe7ff6
Matt Boler <meb0054@auburn.edu>
2022-04-09 16:54:48 -0500

    Highlight column of max line width

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,7 +7,13 @@ local g = vim.g
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+    fn.system({
+        'git', 
+        'clone', 
+        '--depth=1', 
+        'https://github.com/savq/paq-nvim.git', 
+        install_path
+    })
 end
 
 ---- Install plugins
