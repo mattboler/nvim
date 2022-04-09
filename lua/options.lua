80b49ce03034ba5099853d64b4cd0816a16ff2d7
Matt Boler <meb0054@auburn.edu>
2022-04-09 16:53:39 -0500

    Highlight column of max line width

--- lua/options.lua
+++ lua/options.lua
@@ -7,27 +7,26 @@ local g = vim.g
 g.light_colorscheme = 'base16-gruvbox-light-hard'
 g.dark_colorscheme = 'base16-gruvbox-dark-pale'
 
-cmd [[
-    execute 'colorscheme ' .. g:dark_colorscheme
-]]
+cmd [[execute 'colorscheme ' .. g:dark_colorscheme]]
 
-opt.termguicolors = true -- Enable 256-color
-opt.number = true        -- Show line numbers
-opt.scrolloff = 8        -- Rows of context when when scrolling
-opt.sidescrolloff = 8    -- Columns of context when scrolling
-opt.splitbelow = true    -- New hsplits below current window
-opt.splitright = true    -- New vsplits to right of current window
-opt.linebreak = true     -- Wrap on word boundary
+opt.termguicolors = true   -- Enable 256-color
+opt.number = true          -- Show line numbers
+opt.scrolloff = 8          -- Rows of context when when scrolling
+opt.sidescrolloff = 8      -- Columns of context when scrolling
+opt.splitbelow = true      -- New hsplits below current window
+opt.splitright = true      -- New vsplits to right of current window
+opt.linebreak = true       -- Wrap on word boundary
+cmd [[set colorcolumn=80]] -- Highlight max line width
 
 ---- Indentation
-opt.expandtab = true     -- Use spaces instead of tabs
-opt.tabstop = 4          -- A tab is 4 spaces
-opt.shiftwidth = 4       -- An indent is 4 spaces
-opt.shiftround = true    -- Round indents
-opt.smartindent = true   -- Automatically indent
+opt.expandtab = true       -- Use spaces instead of tabs
+opt.tabstop = 4            -- A tab is 4 spaces
+opt.shiftwidth = 4         -- An indent is 4 spaces
+opt.shiftround = true      -- Round indents
+opt.smartindent = true     -- Automatically indent
 
 ---- Searching
-opt.hlsearch = true      -- Highlight matches
-opt.incsearch = true     -- Match as you type
-opt.ignorecase = true    -- Case-insensitive matching
-opt.smartcase = true     -- ... unless you type in mixed case
+opt.hlsearch = true        -- Highlight matches
+opt.incsearch = true       -- Match as you type
+opt.ignorecase = true      -- Case-insensitive matching
+opt.smartcase = true       -- ... unless you type in mixed case
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,6 @@ local cmd = vim.cmd
 local opt = vim.opt
 local g = vim.g
 
-
 ---- Bootstrap plugin manager
 local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 
@@ -18,15 +17,16 @@ paq({
     'savq/paq-nvim'; 
 
     -- Dependencies
-    "nvim-lua/plenary.nvim";        -- Utility functions
-    "kyazdani42/nvim-web-devicons"; -- Filetype icons
+    "nvim-lua/plenary.nvim";               -- Utility functions
+    "kyazdani42/nvim-web-devicons";        -- Filetype icons
 
     -- Common
     'RRethy/nvim-base16';                  -- Colorscheme
     {
         'nvim-treesitter/nvim-treesitter', -- Syntax highlighting
         run=TSUpdate
-    }; 'nvim-lualine/lualine.nvim';        -- Status line
+    }; 
+    'nvim-lualine/lualine.nvim';           -- Status line
     'lewis6991/gitsigns.nvim';             -- Git status icons
     'kyazdani42/nvim-tree.lua';            -- File explorer
     'nvim-telescope/telescope.nvim';       -- Fuzzy-finder
@@ -34,11 +34,11 @@ paq({
     'sunjon/Shade.nvim';                   -- Dim inactive windows
 
     -- Language servers
-    'neovim/nvim-lspconfig';           -- Configure LSPs
-    'williamboman/nvim-lsp-installer'; -- Easy LSP install
+    'neovim/nvim-lspconfig';               -- Configure LSPs
+    'williamboman/nvim-lsp-installer';     -- Easy LSP install
 
     -- Language-specific
-    'JuliaEditorSupport/julia-vim';
+    'JuliaEditorSupport/julia-vim';        -- Julia unicode completion
 })
 
 ---- Plugin configs
