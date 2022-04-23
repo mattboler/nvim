91c4dc1618004b73a429ff1487756df79c3410b5
Matt Boler <meb0054@auburn.edu>
2022-04-23 09:51:49 -0500

    Update to new treesitter

--- lua/configure/treesitter.lua
+++ lua/configure/treesitter.lua
@@ -1,5 +1,10 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained',
+    ensure_installed = {
+        'c', 'cpp', 'python', 'julia',
+        'markdown', 'latex',
+        'make', 'cmake', 'bash',
+        'yaml', 'json'
+    },
     highlight = {
         enable = true
     }
